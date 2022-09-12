10745e0ea00869d39b25fa43db640cea2dbacb2c
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2022-09-12 08:35:22 +0430

    project main sections created

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>homepage</h2>
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>List of Books</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Add Book</a>
+                
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
