6471c993c695590920964b444ddac68011a6e087
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2022-09-15 21:33:09 +0430

    book list added to home page

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [books, setBooks] = useState([
+        { title: 'book 1', body: 'book one body...', author: 'ali', id: 1 },
+        { title: 'book 2', body: 'book two body...', author: 'ahmad', id: 2 },
+        { title: 'book 3', body: 'book three body...', author: 'mahmod', id: 3 },
+    ]);
     return (
         <div className="home">
-            <h2>homepage</h2>
+            {books.map((book) => (
+                <div className="book-preview" key={book.id}>
+                    <h2>{book.title}</h2>
+                    <p>author: {book.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
- 
+
 export default Home;
