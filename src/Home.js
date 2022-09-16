943f2341d635c7c43ebb55a40e3c075f345433cf
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2022-09-16 21:45:09 +0430

    specific author list added to home page

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,12 @@ const Home = () => {
     const [books, setBooks] = useState([
         { title: 'book 1', body: 'book one body...', author: 'ali', id: 1 },
         { title: 'book 2', body: 'book two body...', author: 'ahmad', id: 2 },
-        { title: 'book 3', body: 'book three body...', author: 'mahmod', id: 3 },
+        { title: 'book 3', body: 'book three body...', author: 'ahmad', id: 3 },
     ]);
     return (
         <div className="home">
             <BookList books={books} title="All Books!" />
+            <BookList books={books.filter((book)=>book.author==='ahmad')} title="Ahmad's Books!" />
         </div>
     );
 }
