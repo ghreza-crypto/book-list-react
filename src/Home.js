01c0582e62185d652362994a4b3be0e9518d74e5
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2022-09-17 22:28:09 +0430

    loading message added

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,28 @@ import BookList from "./BookList";
 
 const Home = () => {
     const [books, setBooks] = useState(null);
+    const [isPending, setIsPending] = useState(true);
     // const handleDelete = (id) => {
     //     const newBooks = books.filter(book => book.id !== id);
     //     setBooks(newBooks);
     // }
+
     useEffect(() => {
-        fetch('http://localhost:4000/books')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBooks(data);
-            });
+        setTimeout(() => {
+            fetch('http://localhost:4000/books')
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    setBooks(data);
+                    setIsPending(false);
+                });
+        }, 1000);
     }, []);
+
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {books && <BookList books={books} title="All Books!" />}
             {books && <BookList books={books.filter((book) => book.author === 'ahmad')} title="Ahmad's Books!" />}
         </div>
