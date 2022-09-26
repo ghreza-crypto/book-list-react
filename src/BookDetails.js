ae104da2beebe7ecb15ead1e100c14a5b8b1b153
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2022-09-26 21:32:07 +0430

    book detail page completed

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
-const BookDetails = ()  => {
-    const {id}=useParams();
+const BookDetails = () => {
+    const { id } = useParams();
+    const { data: book, error, isPending } = useFetch('http://localhost:4000/books/' + id);
     return (
         <div className="book-details">
-            <h2>book details-{id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>error</div>}
+            {book && (
+                <article>
+                    <h2>{book.title}</h2>
+                    <p>Written by {book.author}</p>
+                    <div>{book.body}</div>
+                </article>
+            )}
         </div>
     );
 }
- 
+
 export default BookDetails;
