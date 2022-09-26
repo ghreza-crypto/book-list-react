daaa22f60762909ae780a04dd84e25230de21753
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2022-09-26 22:10:45 +0430

    create form added to project

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('ahmad');
     return (
         <div className="create">
             <h2>Add a new Book</h2>
+            <form action="">
+                <label htmlFor="">Book title:</label>
+                <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+
+                <label htmlFor="">Book body:</label>
+                <textarea required value={body} onChange={(e) => setBody(e.target.value)} ></textarea>
+
+                <label htmlFor="">Book author:</label>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="ahmad">Ahmad</option>
+                    <option value="ali">Ali</option>
+                </select>
+                <button>Add book</button>
+            </form>
         </div>
     );
 }
- 
+
 export default Create;
