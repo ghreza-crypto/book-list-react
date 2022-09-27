f01c5c4897f4b0002ed8fc7b91eaa50696fd8105
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2022-09-27 19:57:12 +0430

    store function completed with redirect

--- src/Create.js
+++ src/Create.js
@@ -1,13 +1,31 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('ahmad');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const book = { title, body, author };
+        setIsPending(true);
+        fetch('http://localhost:4000/books', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(book)
+        }).then(() => {
+            setIsPending(false);
+            history.push('/');
+        })
+
+    }
     return (
         <div className="create">
             <h2>Add a new Book</h2>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="">Book title:</label>
                 <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
 
@@ -19,7 +37,8 @@ const Create = () => {
                     <option value="ahmad">Ahmad</option>
                     <option value="ali">Ali</option>
                 </select>
-                <button>Add book</button>
+                {!isPending && <button>Add book</button>}
+                {isPending && <button disabled>Adding book...</button>}
             </form>
         </div>
     );
