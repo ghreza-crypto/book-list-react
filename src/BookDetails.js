3bfd0266ffa9262d50acfc6f8d8fee8188b17d1c
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2022-09-27 20:31:51 +0430

    delete functionality added to book detail section

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,9 +1,18 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const BookDetails = () => {
     const { id } = useParams();
     const { data: book, error, isPending } = useFetch('http://localhost:4000/books/' + id);
+    const history = useHistory();
+    const handleDelete = () => {
+        fetch('http://localhost:4000/books/' + book.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/')
+        })
+    }
     return (
         <div className="book-details">
             {isPending && <div>Loading...</div>}
@@ -13,6 +22,7 @@ const BookDetails = () => {
                     <h2>{book.title}</h2>
                     <p>Written by {book.author}</p>
                     <div>{book.body}</div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>
